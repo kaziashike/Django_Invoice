4cb3da88de4a1b01d4217ba5c69d92f3c002c0c2
Kazi Ashique Elahi <kaziashike@gmail.com>
2025-10-26 00:35:50 +0600

    fix urls

--- invoice_system/invoice/urls.py
+++ invoice_system/invoice/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('all_invoices/', views.get_invoices),
-    path('create_invoice/', views.create_invoice),
-    path('pay_invoice/<int:invoice_id>/', views.pay_invoice),
-    path('single_invoice/<int:invoice_id>/', views.get_single_invoice),
+    path('invoices/', views.get_invoices, name='get_invoices'),
+    path('invoices/<str:invoice_id>/', views.get_single_invoice, name='get_single_invoice'),
+    path('invoices/create/', views.create_invoice, name='create_invoice'),
+    path('invoices/<str:invoice_id>/pay/', views.pay_invoice, name='pay_invoice'),
 ]
