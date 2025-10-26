e6fc69495ae8b6f656c05ba827f9d104630867c9
Kazi Ashique Elahi <kaziashike@gmail.com>
2025-10-26 12:42:41 +0600

    Configured for Server, requirements updated

--- invoice_system/invoice_system/settings.py
+++ invoice_system/invoice_system/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hd1+0lzp@ylfekj75)a*7@mi8$c-fhypwq%eq*3g&f%@2^5wvw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
