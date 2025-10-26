28d369871ac950ab2b71d92d54f6a1b63a8cb7f9
Kazi Ashique Elahi <kaziashike@gmail.com>
2025-10-26 07:12:21 +0600

    api working great ***

--- invoice_system/invoice/serializer.py
+++ invoice_system/invoice/serializer.py
@@ -9,26 +9,33 @@ class ItemSerializer(serializers.ModelSerializer):
 
 
 class InvoiceSerializer(serializers.ModelSerializer):
-    items = ItemSerializer(many=True, read_only=True)
+    items = ItemSerializer(many=True)
     class Meta:
         model = Invoice
         fields = '__all__'
         read_only_fields = ['amount']
 
-    def save(self, **kwargs):
-        total_amount=0
-        invoice=super().save(**kwargs)
-        item_data=self.validated_data.get('items')
-        if not item_data:
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        if not items_data:
             raise serializers.ValidationError({"items": "Invoice must have at least one item."})
-        
-        for item in item_data:
-            total_price=item['item_unit_price']*item['quantity']
-            total_amount+=total_price
 
-            Items.objects.create(invoice= invoice, invoice_item=item.get('invoice_item'), item_unit_price=item.get('item_unit_price'), quantity=item.get('quantity'), total=total_price)
+        invoice = Invoice.objects.create(**validated_data)
 
-        invoice.amount=total_amount
+        total_amount = 0
+        for item in items_data:
+            total_price = item['item_unit_price'] * item['quantity']
+            total_amount += total_price
+
+            Items.objects.create(
+                invoice=invoice,
+                invoice_item=item['invoice_item'],
+                item_unit_price=item['item_unit_price'],
+                quantity=item['quantity'],
+                total=total_price
+            )
+
+        invoice.amount = total_amount
         invoice.save()
         return invoice
 
